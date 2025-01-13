9ddf5ea37541279727cab42e53f2ce19821d07ea
bharat lal <127212425+bharatlal124@users.noreply.github.com>
2025-01-13 14:42:55 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -19,6 +19,15 @@ const firebaseConfig = {
   messagingSenderId: "152051881154",
   appId: "1:152051881154:web:6a2398c8312560cada975b",
   measurementId: "G-NLN3KBDHVY",
+
+
+  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
 };
 
 console.log(process.env);
